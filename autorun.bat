4aaf0a63a6092880820d06890219301953fb83c3
adrien <adrien.jean1989@gmail.com>
2024-02-08 14:12:23 +0100

    add autorun.bat

---
+++ autorun.bat
@@ -0,0 +1,15 @@
+docker network create --driver=bridge production-network
+sleep 5
+
+cd Mariadb 
+docker compose up -d
+
+cd ..
+cd Mongodb 
+docker compose up -d
+
+cd ..
+cd Traefik
+docker compose up -d
+
+cd ..
