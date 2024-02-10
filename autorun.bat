c05b4cc62bc30b6bc9874d9f854e44472d6a8a66
ajean1989 <105272808+ajean1989@users.noreply.github.com>
2024-02-10 18:54:59 +0100

    actions

--- autorun.bat
+++ autorun.bat
@@ -1,19 +1,18 @@
 docker network create --driver=bridge production-network
-sleep 5
 
 cd Mariadb 
-docker compose up --build -d
+docker compose up --build -d --force-recreate
 
 cd ..
 cd Mongodb 
-docker compose up --build -d
+docker compose up --build -d --force-recreate
 
 cd ..
 cd Traefik
-docker compose up --build -d
+docker compose up --build -d --force-recreate
 
 cd ..
 cd init
-docker compose up --build -d
+docker compose up --build -d --force-recreate
 
 cd ..
