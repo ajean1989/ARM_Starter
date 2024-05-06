8f59c4b91408819307373f06442d9922e1a2fbca
adrien <adrien.jean1989@gmail.com>
2024-05-06 23:59:59 +0200

    config dev/prod https tests

--- tests/tests/test_traefik.py
+++ tests/tests/test_traefik.py
@@ -1,6 +1,9 @@
 # Tester toutes routes (réponse 200)
 
 import httpx
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def test_adminer():
     response = httpx.get('https://traefik/adminer/')
